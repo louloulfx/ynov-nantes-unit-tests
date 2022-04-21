a5ddeb6136a60d31b3f8897269015dfac1f5af70
Valentin <valentinguibert@hotmail.fr>
2022-04-21 15:57:55 +0200

    feat(unit-test): unit test on TempTracker

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,37 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEqual(42, 42);
+    public function test_insert__one_temp() {
+        $tracker = new TempTracker();
+        $tracker->insert(50);
+        $this->assertEquals(50, $tracker->get_max());
+        $this->assertEquals(50, $tracker->get_min());
+        $this->assertEquals(50, $tracker->get_mean());
+    }
+
+    public function test_insert__many_temps() {
+        $tracker = new TempTracker();
+        $tracker->insert(50);
+        $tracker->insert(100);
+        $tracker->insert(75);
+        $this->assertEquals(100, $tracker->get_max());
+        $this->assertEquals(50, $tracker->get_min());
+        $this->assertEquals(75, $tracker->get_mean());
+    }
+
+    public function test_insert__many_temps_with_same_temp() {
+        $tracker = new TempTracker();
+        $tracker->insert(50);
+        $tracker->insert(50);
+        $tracker->insert(50);
+        $this->assertEquals(50, $tracker->get_max());
+        $this->assertEquals(50, $tracker->get_min());
+        $this->assertEquals(50, $tracker->get_mean());
+    }
+
+    public function test__insert_failure() {
+            $tracker = new TempTracker();
+            $this->expectException(TypeError::class);
+            $tracker->insert('a');
     }
 }
