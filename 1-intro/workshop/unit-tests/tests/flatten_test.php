3505a890a1ec1eb5a75e3c15fa284299bf8f801c
louloulfx <louloulfx1@hotmail.fr>
2022-04-21 15:42:59 +0200

    feat(units-test): flatten_test.php

--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,23 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
-    public function test_something()
+    public function test__flatten_array_two_dimensions()
     {
-        $this->assertEqual(1337, 1337);
+        $array = [1, 2, [3, 4], 5, [6, 7]];
+        $expected = [1, 2, 3, 4, 5, 6, 7];
+        $this->assertEquals($expected, flatten($array));
+    }
+
+    public function test__flatten_array_three_dimensions()
+    {
+        $array = [1, 2, [3, 4], 5, [6, [7, 8, [9, 10]]]];
+        $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], flatten($array));
+    }
+
+    public function test__flatten_failure_not_array()
+    {
+        $string = "string";
+        $this->expectException(TypeError::class);
+        flatten($string);
     }
 }
