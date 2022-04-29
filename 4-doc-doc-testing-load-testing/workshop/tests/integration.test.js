e83cbfbeb07938c5083a0811d4c8e4da43a388c5
louloulfx <louloulfx1@hotmail.fr>
2022-04-29 14:52:33 +0200

    feat(wk-4): tests inte

---
+++ 4-doc-doc-testing-load-testing/workshop/tests/integration.test.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+const ToDo = require('../toDoModel').ToDo;
+
+beforeAll(async () => {
+    await mongoose
+        .connect("mongodb://localhost:27017/toDoApp", {
+            useNewUrlParser: true,
+        })
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch(err => {
+            console.log("Could not connect to MongoDB");
+            console.error(err);
+        });
+});
+
+it("Delete Todo", async () => {
+    const todoText = "Test todo";
+
+    const newTodo = new ToDo({
+        text: todoText
+    });
+
+    await newTodo.save();
+
+    await ToDo.deleteMany({}, (err) => {
+        if (err) console.log(`error: ${err}`);
+    });
+
+    const todo = await ToDo.find({}, (err, docs) => {
+        if (err) console.log(`error: ${err}`);
+        else return docs;
+    });
+
+    expect(todo.length).toBe(0);
+})
+
+it("Create Todo", async () => {
+    const todoText = "Test todo";
+
+    const newTodo = new ToDo({
+        text: todoText
+    });
+
+    await newTodo.save();
+
+    const todo = await ToDo.find({}, (err, docs) => {
+        if (err) console.log(`error: ${err}`);
+        else return docs;
+    });
+
+    expect(todo.length).toBe(1);
+    expect(todo[0].text).toBe(todoText);
+})
+
+it("Update Todo", async () => {
+    await ToDo.deleteMany({}, (err) => {
+        if (err) console.log(`error: ${err}`);
+    });
+    const todoText = "Test todo";
+    const updatedTodoText = "Updated test todo";
+
+    const newTodo = new ToDo({
+        text: todoText
+    });
+
+    await newTodo.save();
+
+    await ToDo.updateOne({ text: todoText }, { text: updatedTodoText }, (err) => {
+        if (err) console.log(`error: ${err}`);
+    });
+
+    const todo = await ToDo.find({}, (err, docs) => {
+        if (err) console.log(`error: ${err}`);
+        else return docs;
+    });
+
+    expect(todo.length).toBe(1);
+    expect(todo[0].text).toBe(updatedTodoText);
+})
+
